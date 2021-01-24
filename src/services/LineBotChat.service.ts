1055bc3f58591e85a233cd3cdb5bed9ea6398fe6
pacharapold <pacharapol@midassoft.dev>
2021-01-24 20:54:17 +0700

    Update LineBotChat.service.ts

--- src/services/LineBotChat.service.ts
+++ src/services/LineBotChat.service.ts
@@ -44,79 +44,83 @@ export default {
     const flex = [
       {
         type: 'flex',
-        size: 'giga',
-        direction: 'ltr',
-        body: {
-          type: 'box',
-          layout: 'horizontal',
-          contents: [
-            {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'icon',
-                  url: `${profile.pictureUrl}`,
-                  align: 'start',
-                  aspectMode: 'cover',
-                },
-              ],
-            },
-            {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'box',
-                  layout: 'horizontal',
-                  contents: [
-                    {
-                      type: 'text',
-                      text: 'คุณ',
-                    },
-                    {
-                      type: 'text',
-                      text: `${profile.displayName}`,
-                    },
-                  ],
-                },
-                {
-                  type: 'box',
-                  layout: 'horizontal',
-                  contents: [
-                    {
-                      type: 'text',
-                      text: 'ยอดเงิน',
-                    },
-                    {
-                      type: 'text',
-                      text: `${gm.balance}`,
-                    },
-                  ],
-                },
-                {
-                  type: 'box',
-                  layout: 'horizontal',
-                  contents: [
-                    {
-                      type: 'text',
-                      text: 'username',
-                    },
-                    {
-                      type: 'text',
-                      text: `${gm.username}`,
-                    },
-                  ],
-                },
-              ],
-              justifyContent: 'center',
-            },
-          ],
-        },
-        action: {
-          type: 'message',
-          label: 'profile',
-          text: 'profile',
+        altText: 'Flex Message',
+        contents: {
+          type: 'bubble',
+          size: 'giga',
+          direction: 'ltr',
+          body: {
+            type: 'box',
+            layout: 'horizontal',
+            contents: [
+              {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'icon',
+                    url: `${profile.pictureUrl}`,
+                    align: 'start',
+                    aspectMode: 'cover',
+                  },
+                ],
+              },
+              {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: 'คุณ',
+                      },
+                      {
+                        type: 'text',
+                        text: `${profile.displayName}`,
+                      },
+                    ],
+                  },
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: 'ยอดเงิน',
+                      },
+                      {
+                        type: 'text',
+                        text: `${gm.balance}`,
+                      },
+                    ],
+                  },
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: 'username',
+                      },
+                      {
+                        type: 'text',
+                        text: `${gm.username}`,
+                      },
+                    ],
+                  },
+                ],
+                justifyContent: 'center',
+              },
+            ],
+          },
+          action: {
+            type: 'message',
+            label: 'profile',
+            text: 'profile',
+          },
         },
       },
     ];
